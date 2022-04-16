b0408449fac2646d69db02d13b76cd6e05365542
Abzed <ibraabzed@gmail.com>
2022-04-16 19:58:04 +0300

    commit tree f9d36d92ad3c2ab1475b137f11aff6f05a06ffda

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here!
+let remMain = document.getElementById('main')
+remMain.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Abzed is the champion'
+
+let body = document.querySelector('body')
+
+body.append(newHeader)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
